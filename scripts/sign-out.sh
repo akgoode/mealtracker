664c2519243b64bf2944ba4683b0da7dda3d8568
akgoode <akgoode@gmail.com>
2017-02-14 20:32:12 -0500

    Fix sign-out script
    Did not have the right API path

--- scripts/sign-out.sh
+++ scripts/sign-out.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# curl "http://localhost:3000/sign-out/$ID" \
-curl "${API}${API_PATH}/${ID}" \
+API="${API_ORIGIN:-http://localhost:4741}"
+URL_PATH="/sign-out"
+curl "${API}${URL_PATH}/${ID}" \
   --include \
   --request DELETE \
-  --header "Authorization: Token token=${TOKEN}" \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=$TOKEN"
 
-
-# data output from curl doesn't have a trailing newline
 echo
